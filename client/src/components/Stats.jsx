e84656657f54bce04cf49573ad88353beb188a56
Ujjwal Gupta <guptaujjwal259@gmail.com>
2024-08-20 18:19:49 +0530

    changed stats

--- client/src/components/Stats.jsx
+++ client/src/components/Stats.jsx
@@ -13,7 +13,7 @@ const Stats = () => {
   useEffect(() => {
     const fetchStats = async () => {
       try {
-        const response = await fetch('http://localhost:3000/stripe/api/stats ');
+        const response = await fetch('${import.meta.env.VITE_APP_BACKEND_URL}/stripe/api/stats ');
         
         if (!response.ok) {
           throw new Error('Network response was not ok');
